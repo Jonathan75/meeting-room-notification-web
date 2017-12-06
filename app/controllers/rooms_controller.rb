fbfc21771ec8472b3cea66bac9429ef2f8c55ba4
Joel Byler <jbyler@covermymeds.com>
2017-12-06 10:23:42 -0500

    add qr code

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,21 @@
 class RoomsController < ApplicationController
+  include ActionController::MimeResponds
   def index
     @rooms = Room.all
   end
 
   def show
-    room = Room.find(params[:id])
-    room.update_attribute(:status, 0) if room.updated_at < 30.seconds.ago
-    render json: {status: room.status}
+    @room = Room.find(params[:id])
+    @room.update_attribute(:status, 0) if @room.updated_at < 30.seconds.ago
+
+    respond_to do |format|
+      format.json do
+        render json: {status: @room.status}
+      end
+      format.html do
+        render :edit
+      end
+    end
   end
 
   def edit
